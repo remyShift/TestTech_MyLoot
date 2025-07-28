e792ce446a1b508f2b24104ad3e8ddad82c6f03c
remyShift <remy.engassac@icloud.com>
2025-07-28 21:29:25 +0200

    refactor: rename getTeamsInfo to getRawStatsForTeam in TeamStatsService and update related tests for consistency

--- server/src/services/teamStatsService.ts
+++ server/src/services/teamStatsService.ts
@@ -4,6 +4,20 @@ interface TeamMember {
 	totalCoins: number;
 }
 
+interface TeamMemberWithPercent extends TeamMember {
+	percent: number;
+}
+
+interface TeamStats {
+	total: number;
+	members: TeamMember[];
+}
+
+interface TeamStatsWithPercent {
+	total: number;
+	members: TeamMemberWithPercent[];
+}
+
 export interface TeamStatsRepository {
 	getTeamMembers(teamId: number): Promise<TeamMember[]>;
 }
@@ -11,7 +25,7 @@ export interface TeamStatsRepository {
 export class TeamStatsService {
 	constructor(private readonly teamStatsRepository: TeamStatsRepository) {}
 
-	async getStatsForTeam(teamId: number) {
+	async getStatsForTeam(teamId: number): Promise<TeamStatsWithPercent> {
 		const membersWithTotal = await this.getSortedStatsForTeam(teamId);
 
 		const total = membersWithTotal.total;
@@ -28,7 +42,7 @@ export class TeamStatsService {
 		};
 	}
 
-	async getTeamsInfo(teamId: number) {
+	async getRawStatsForTeam(teamId: number): Promise<TeamStats> {
 		const members = await this.teamStatsRepository.getTeamMembers(teamId);
 
 		return {
@@ -40,8 +54,8 @@ export class TeamStatsService {
 		};
 	}
 
-	async getSortedStatsForTeam(teamId: number) {
-		const membersWithTotal = await this.getTeamsInfo(teamId);
+	async getSortedStatsForTeam(teamId: number): Promise<TeamStats> {
+		const membersWithTotal = await this.getRawStatsForTeam(teamId);
 
 		const sortedMembers = membersWithTotal.members.sort(
 			(a, b) => b.totalCoins - a.totalCoins
