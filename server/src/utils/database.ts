701282997ad585cc0459d7c900a56b3983939cd3
remyShift <remy.engassac@icloud.com>
2025-07-29 19:33:06 +0200

    feat: add express-rate-limit package and implement unique constraint on Team name with date index in CoinEarning model

---
+++ server/src/utils/database.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+	var __prisma: PrismaClient | undefined;
+}
+
+export const prisma = global.__prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+	global.__prisma = prisma;
+}
+
+process.on('beforeExit', async () => {
+	await prisma.$disconnect();
+});
